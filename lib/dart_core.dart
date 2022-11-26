7acbbe20d13b043418b02671685f21b59dab359d
Svetozar <swetozar.volkov@yandex.ru>
2022-11-26 11:30:44 +0400

    Remove core trace

--- lib/dart_core.dart
+++ lib/dart_core.dart
@@ -4,7 +4,6 @@ export 'package:collection/collection.dart';
 export 'package:meta/meta.dart';
 
 export 'src/bootstrap.dart';
-export 'src/core_trace.dart';
 export 'src/disposable.dart';
 export 'src/exception/chained_exception.dart';
 export 'src/opt.dart';
